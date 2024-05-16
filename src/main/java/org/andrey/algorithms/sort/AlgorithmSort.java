6bb5fe9f6e60b896f7cbfac604e1963bda82c634
Andrey <honor8x98@gmail.com>
2024-05-16 17:11:33 +0300

    Insertion Sort

--- src/main/java/org/andrey/algorithms/sort/AlgorithmSort.java
+++ src/main/java/org/andrey/algorithms/sort/AlgorithmSort.java
@@ -31,4 +31,15 @@ public class AlgorithmSort {
             array[i] = min;
         }
     }
+
+    static void insertionSort(int[] sortArr) {
+        int j;
+        for (int i = 1; i < sortArr.length; i++) {
+            int swap = sortArr[i];
+            for (j = i; j > 0 && swap < sortArr[j - 1]; j--) {
+                sortArr[j] = sortArr[j - 1];
+            }
+            sortArr[j] = swap;
+        }
+    }
 }
--- src/main/java/org/andrey/algorithms/sort/AlgorithmSortAnalysis.java
+++ src/main/java/org/andrey/algorithms/sort/AlgorithmSortAnalysis.java
@@ -2,8 +2,7 @@ package org.andrey.algorithms.sort;
 
 import java.util.Arrays;
 
-import static org.andrey.algorithms.sort.AlgorithmSort.bubbleSort;
-import static org.andrey.algorithms.sort.AlgorithmSort.selectionSort;
+import static org.andrey.algorithms.sort.AlgorithmSort.*;
 
 public class AlgorithmSortAnalysis {
     private static final int ARRAY_SIZE = 100;
@@ -11,9 +10,9 @@ public class AlgorithmSortAnalysis {
     public static void main(String[] args) {
 //        int[] array = {4, 7, 5, 8, 1, 3, 6, 9, 0, 2};
 
-        selectionSortPrint();
-
-        bubbleSortPrint();
+        bubbleSortPrint(); // O(n^2)
+        selectionSortPrint(); // O(n^2)
+        insertionSortPrint(); // O(n^2)
     }
 
     private static void bubbleSortPrint() {
@@ -28,6 +27,14 @@ public class AlgorithmSortAnalysis {
         int[] array = getRandomArray();
         long currentTime = System.currentTimeMillis();
         selectionSort(array);
+        long leadTime = System.currentTimeMillis() - currentTime;
+        System.out.printf("Execution time %dms\n %s\n", leadTime, Arrays.toString(array));
+    }
+
+    private static void insertionSortPrint() {
+        int[] array = getRandomArray();
+        long currentTime = System.currentTimeMillis();
+        insertionSort(array);
         long leadTime = System.currentTimeMillis() - currentTime;
         System.out.printf("Execution time %dms\n %s\n", leadTime, Arrays.toString(array));
     }
